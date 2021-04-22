19cd9f2a1e680108c71f3703c5105e863c41e99e
Wescley <wescley13sri@gmail.com>
2021-04-22 09:22:18 -0300

    Copy Constructor

---
+++ src/com/wcaldeira/copyconstructor/Demo.java
@@ -0,0 +1,16 @@
+package com.wcaldeira.copyconstructor;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Employee john = new Employee("John", new Address("123 London Road", "London", "UK"));
+
+        Employee chris = new Employee(john);
+
+        chris.name = "Chris";
+
+        System.out.println(john);
+        System.out.println(chris);
+    }
+
+}
